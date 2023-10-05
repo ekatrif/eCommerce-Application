b1da6095a30305c2555a05f800dd39ec1fb1f7ea
ekatrif <katiapervak@gmail.com>
2023-10-05 14:49:18 +0300

    fix: add new projectKey

--- src/api/env.ts
+++ src/api/env.ts
@@ -1,8 +1,8 @@
 const ENV = {
   Host: 'https://api.us-central1.gcp.commercetools.com',
-  ProjectKey: 'tourist-goods-store',
-  Client_id: 'XKaW2tUBFkhwiyrOcK_p3p43',
-  Client_secret: 'qDQ6qCyTwyL1FLzt-ytyG9vOAskBKEE6',
+  ProjectKey: 'goods-for-tourism-store',
+  Client_id: 'urXFuWUe02KFNAOsv3QyfW4t',
+  Client_secret: 'BaS5ZMl_0ptXNN66AkICY8XT3gAIkLvK',
 };
 
 export default ENV;
